bcec129c8eb925253a29dcb1946933a3b1db42a1
honzatomek <rpi3.tomek@protonmail.com>
2020-04-12 11:48:50 +0200

    sonoff and wifi wrapped in class, not working

--- config_example.py
+++ config_example.py
@@ -1,17 +1,24 @@
 # example of config.py file, input correct values and rename
+NAME = 'sonoff'
+
+WIFI_HOST = '<wifi_hostname>'
 WIFI_SSID = '<wifi_ssid>'
 WIFI_PASS = '<wifi_password>'
-WIFI_HOST = '<base_hostname>'
 
 MQTT_CLIENT_ID = WIFI_HOST
 MQTT_BROKER = 'xxx.xxx.xxx.xxx'
 MQTT_PORT = 1883
-MQTT_UNAME = WIFI_HOST
-MQTT_PWD = '<mqtt_password>'
-
-TOPIC_IN = (MQTT_CLIENT_ID + '/in').encode()
-TOPIC_OUT = (MQTT_CLIENT_ID + '/out').encode()
+MQTT_UNAME = None
+MQTT_PASS = None
+MQTT_DELAY = 1000
 
 LED = {'gpio': 13, 'on': 0}
 RELAY = {'gpio': 12, 'on': 1}
 BUTTON = {'gpio': 0, 'on': 0}
+RTIMER_DELAY = 250
+BLINK_COUNT = 1
+BLINK_DELAY = 250
+RSAVE = 'relay.save'
+WIFI_DELAY = 3000
+
+
--- main.py
+++ main.py
@@ -1,128 +1,167 @@
-# ---------------------------------- micropython modules ---
+# ---------------------------------- micropython modules ---            # {{{1
 import machine
 import network
+import uos
+import ubinascii
 from utime import sleep_ms
-from umqtt.simple import MQTTClient
+from umqtt.robust import MQTTClient
 
-
-# --------------------------------------- custom modules ---
+# --------------------------------------- custom modules ---            # {{{1
 from config import *
 
 
+# ---------------------------------------------- classes ---
+class WiFi:                                                             # {{{1
+    def __init__(self, ssid, password, hostname):                       # {{{2
+        network.WLAN(network.AP_IF).active(False)
+
+        self.ssid = ssid
+        self.pwd = password
+
+        self.sta = network.WLAN(network.STA_IF)
+        self.mac = ubinascii.hexlify(network.WLAN().config('mac'),'').decode()
+        self.hostname = hostname + self.mac[-4:]
+
+        self.sta.config(dhcp_hostname=hostname + self.mac[-4:])
+
+    def connect(self, delay=200, blink=None):                           # {{{2
+        for i in range(5):
+            if not self.sta.isconnected():
+                if not self.sta.active():
+                    self.sta.active(True)
+                self.sta.connect(self.ssid, self.pwd)
+                sleep_ms(delay)
+            else:
+                if blink is not None:
+                    blink(3, 250)
+                return True
+        if blink is not None:
+            blink(1, 2000)
+        return False
+
+
+class Sonoff:                                                           # {{{1
+    def __init__(self, name='sonoff'):                                  # {{{2
+        self.name = name
+
+        self.led = machine.Pin(LED['gpio'],
+                               machine.Pin.OUT,
+                               value=(1 - LED['on']))
+
+        self.button = machine.Pin(BUTTON['gpio'], machine.Pin.IN)
+        self.button.irq(trigger=machine.Pin.IRQ_FALLING,
+                        handler=self.button_cb)
+
+        self.relay_value = self.load_state()
+        self.relay = machine.Pin(RELAY['gpio'],
+                                 machine.Pin.OUT,
+                                 value=self.relay_value)
+
+        self.timer = machine.Timer(0)
+        self.timer.init(period=RTIMER_DELAY,
+                        mode=machine.Timer.PERIODIC,
+                        callback=self.check)
+
+        self.wifi = None
+        self.mqtt = None
+
+    def blink(self, count=BLINK_COUNT, delay=BLINK_DELAY):  # {{{2
+        for i in range(2 * count - 1):
+            self.led.value(not self.led.value())
+            sleep_ms(delay)
+        self.led.value(1 - LED['on'])
+
+    def button_cb(self, pin=None):                                      # {{{2
+        bval = self.button.value()
+        for i in range(11):
+            sleep_ms(250)
+            if self.button.value() != bval:
+                self.blink()
+                self.switch()
+                return
+        self.reset()
+
+    def load_state(self):                                               # {{{2
+        try:
+            with open(RSAVE, 'r') as rst:
+                return int(rst.readline().strip('\n'))
+        except Exception as e:
+            self.relay_value = 0
+            self.save_state()
+            return 0
+
+    def save_state(self):                                               # {{{2
+        with open(RSAVE, 'w') as rst:
+            rst.write(str(self.relay_value))
+
+    def check(self, timer=None):                                                    # {{{2
+        if self.relay.value() != self.relay_value:
+            self.relay.value(self.relay_value)
+
+    def switch(self, value=None):                                       # {{{2
+        if value is None or value == 2:
+            self.relay.value(not self.relay.value())
+        elif value == 1:
+            self.relay.value(RELAY['on'])
+        elif value == 0:
+            self.relay.value(1 - RELAY['on'])
+        self.relay_value = self.relay.value()
+        if self.mqtt is not None:
+            mqtt_publish(self.relay_value)
+
+    def reset(self):                                                    # {{{2
+        self.save_state()
+        self.blink(3)
+        machine.reset()
+
+    def connect(self):
+        pass
+
+
 # -------------------------------------------- functions ---
-def gpio(gpio_pin=13, value='toggle', count=3, sleep=500):
-    if isinstance(gpio_pin, int):
-        pin = gpio_pin
-        on = 0
-    elif isinstance(gpio_pin, dict):
-        pin = gpio_pin['gpio']
-        on = gpio_pin['on']
-
-    p = machine.Pin(pin, machine.Pin.OUT, value=machine.Pin(pin).value())
-    if value == 'on':
-        p.value(on)
-    elif value == 'toggle':
-        p.value(not p.value())
-    elif value == 'blink':
-        p.value(on)
-        for i in range(count * 2 - 1):
-            sleep_ms(sleep)
-            p.value(not p.value())
-    else:
-        p.value(1 - on)
-
-    return 'on' if p.value() == on else 'off'
-
-
-def isconnected():
-    return network.WLAN(network.STA_IF).isconnected()
-
-
-def connect():
-    network.WLAN(network.AP_IF).active(False)
-    sta = network.WLAN(network.STA_IF)
-    sta.config(dhcp_hostname=WIFI_HOST)
-    sta.active(True)
-    sta.connect(WIFI_SSID, WIFI_PASS)
-
-#     for t in range(10):
-#         sleep_ms(10 * t)
-#         if sta.isconnected():
-#             gpio(LED, 'blink', 3, 500)
-#             return True
-#
-#     gpio(LED, 'on')
-#     return False
-
-
-def relay_state():
-    return 'on' if machine.Pin(RELAY['gpio']).value() == RELAY['on'] else 'off'
-
-
-def reset(pin=None):
-    with open('relay_state', 'w') as rst:
-        rst.write(relay_state())
-
-    gpio(LED, 'blink', 1, 1000)
-    machine.reset()
-
-
-def timeout_callback(t):
-    if isconnected():
-        gpio(LED, 'blink', 1, 250)
-    else:
-        gpio(LED, 'on')
-        connect()
-
-
-def subscribe_callback(topic, msg):
-    retval = None
+def mqtt_subscribe(topic, msg):                                         # {{{1
+    global s
     msg = msg.decode()
-    if msg == 'blink':
-        gpio(LED, 'blink')
-    elif msg in ['on', 'off', 'toggle']:
-        retval = gpio(RELAY, msg)
+    if msg in [0, 1, 2]:
+        s.switch(msg)
     elif msg == 'state':
-        retval = relay_state()
+        mqtt_publish(s.relay_value)
+    elif msg == 'blink':
+        s.blink(3, 250)
     elif msg == 'reset':
-        reset()
-
-    if retval is not None:
-        publish(retval)
+        s.reset()
 
 
-def publish(value):
-    global mqtt
-    mqtt.publish(TOPIC_OUT, value.encode())
+def mqtt_publish(message):                                              # {{{1
+    global s
+    topic = s.mqtt.client_id + '/out'
+    s.mqtt.publish(topic.encode(), message.encode())
 
 
-# ---------------------------------------------- program ---
-with open('relay_state', 'r') as rst:
-    gpio(RELAY, rst.readline().strip('\n'))
-
+# ---------------------------------------------- program ---            # }}}1
 sleep_ms(1000)
+s = Sonoff(NAME)
+s.load_state()
+w = WiFi(WIFI_SSID, WIFI_PASS, s.name)
+w.connect(blink=s.blink)
+WIFI_HOST = w.hostname
+MQTT_CLIENT_ID = w.hostname
 
-connect()
-while not isconnected():
-    sleep_ms(100)
-gpio(LED, 'blink')
+s.mqtt = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT,
+                    user=MQTT_UNAME, password=MQTT_PASS)
+s.mqtt.set_callback(mqtt_subscribe)
+s.mqtt.connect(clean_session=False)
+TOPIC_IN = s.mqtt.client_id + '/in'
+s.mqtt.subscribe(TOPIC_IN.encode())
 
-t = machine.Timer(0)
-t.init(period=5000, mode=machine.Timer.PERIODIC, callback=timeout_callback)
-
-r = machine.Pin(BUTTON['gpio'], machine.Pin.IN)
-r.irq(trigger=machine.Pin.IRQ_FALLING, handler=reset)
-
-mqtt = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER)
-mqtt.set_callback(subscribe_callback)
-mqtt.connect()
-mqtt.subscribe(TOPIC_IN)
-
-publish(relay_state())
+mqtt_publish(s.relay_value)
 while True:
     try:
-        mqtt.check_msg()
-        sleep_ms(100)
+        s.mqtt.check_msg()
+        sleep_ms(MQTT_DELAY)
     except Exception as e:
-        reset()
+        break
 
+s.reset()
+
+# vim:fdm=marker:fdl=1:
