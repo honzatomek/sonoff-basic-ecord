b34a2c2a1aa00551b8e4e9809700381fc48a5b62
honzatomek <rpi3.tomek@protonmail.com>
2020-05-15 20:05:52 +0200

    debugged working version

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import machine
-from wlan import *
-from config import *
+import utime
+from wlan import WLAN, WLANException
+from config import WLAN_SSID, WLAN_PASS, WLAN_HOST, WLAN_RTRY, WLAN_DLAY
 
-# uncomment if not located in boot.py for webrepl access, the access needs to be set up previously
+# uncomment if not located in boot.py for webrepl access, the access needs to
+# be set up previously
 # import webrepl
 # webrepl.start()
 
@@ -10,35 +12,37 @@ from config import *
 button_pressed = (machine.Pin(0, machine.Pin.IN).value() == 0)
 
 # connect to wifi
-wlan = WLAN(WLAN_HOST, WLAN_PASS)
+wlan = WLAN(WLAN_SSID, WLAN_PASS)
 wlan.set_hostname(WLAN_HOST)
-for i < WLAN_RTRY:
-  try:
-    if wlan.connect(delay=WLAN_DLAY):
-      print('[+] WLAN connected to {0} as {1}.'.format(WLAN_SSID, WLAN.ip()))
-      # blink 3 times to indicate wifi connected
-      for i in range(3 * 2 + 1):
-        machine.Pin(13).value(i % 2)
-        utime.sleep_ms(250)
-      break
-  except WLANException as e:
-    print(e)
-    print('[-] WLAN connection {0} to {1} not successful..'.format(i, WLAN_SSID))
-  except Exception as e:
-    print('[-] Unhandled Exception occured:\n    {0}\n    Resetting!'.format(e))
-    machine.reset()
+i = 1
+while i < WLAN_RTRY:
+    i += 1
+    try:
+        if wlan.connect(delay=WLAN_DLAY):
+            print('[+] WLAN connected to {0} as {1}.'.format(WLAN_SSID, wlan.ip()))
+        # blink 3 times to indicate wifi connected
+        for i in range(3 * 2 + 1):
+            machine.Pin(13).value(i % 2)
+            utime.sleep_ms(250)
+        break
+    except WLANException as e:
+        print('[-] Main: {0}'.format(e))
+        print('[-] WLAN connection {0} to {1} not successful..'.format(i, WLAN_SSID))
+    except Exception as e:
+        print('[-] Unhandled Exception occured:\n{0}\n[-] Resetting!'.format(e))
+        machine.reset()
 
 # check if button was pressed upon boot
 if not button_pressed:
-  print('[+] Starting Sonoff Switch program..')
-  try:
-    import sonoff
-  except KeyboardInterrupt as e:
-    print('[-] Code run interrupted, exception:\n    {0}'.format(e))
-  except Exception as e:
-    print('[-] Unhandled Exception occured:\n    {0}\n    Resetting!'.format(e))
-    machine.reset()
+    print('[+] Starting Sonoff Switch program..')
+    try:
+        import sonoff
+    except KeyboardInterrupt as e:
+        print('[i] Code run interrupted.')
+    except Exception as e:
+        print('[-] Unhandled Exception occured:\n{0}\n[-] Resetting!'.format(e))
+        machine.reset()
 else:
-  print('[i] Button pressed upon boot.')
+    print('[i] Button pressed upon boot.')
 
 print('[i] Entering REPL mode..')
--- sonoff.py
+++ sonoff.py
@@ -1,106 +1,271 @@
-from machine import Pin, Timer, reset
-from utime import sleep_ms
+import machine
+import utime
 from uos import listdir
 from umqtt.robust import MQTTClient
+import gc
+from wlan import WLAN
+import builtins
 
-_PIN_LED = const(13)
-_PIN_BTN = const(0)
-_PIN_REL = const(12)
+from config import DEBUG
+from config import WLAN_SSID, WLAN_PASS, WLAN_DLAY
+from config import MQTT_BRKR, MQTT_PORT, MQTT_TPIN, MQTT_TPOU
 
-_LED_ON = const(0)
-_BTN_ON = const(0)
-_REL_ON = const(1)
+
+def print(*args, **kwargs):
+    if DEBUG:
+        builtins.print(*args, **kwargs)
+
+
+def eprint(*args, **kwargs):
+    builtins.print(*args, **kwargs)
+
+
+def load(save_file='relay.save', default=0):
+    try:
+        if save_file in listdir('/'):
+            with open(save_file, 'r', encoding='utf-8') as save:
+                return int(save.readline())
+        else:
+            return default
+    except Exception as e:
+        eprint('[-] load: Exception occured\n    {0}'.format(e))
+        return default
+
+
+def save(value, save_file='relay.save'):
+    try:
+        with open(save_file, 'w', encoding='utf-8') as save:
+            save.write(str(value))
+        return True
+    except Exception as e:
+        eprint('[-] save: Exception occured\n    {0}'.format(e))
+        return False
+
 
 class SonoffException(Exception):
-  pass
+    pass
+
+
+class Relay:
+    def __init__(self, pin=12, on=1, value='load'):
+        self.__on = on
+        self.__off = 1 - on
+        if value == 'load':
+            self.__pin = machine.Pin(pin, machine.Pin.OUT,
+                                     value=load(default=self.__off))
+        elif value == 'state':
+            self.__pin = machine.Pin(pin, machine.Pin.OUT,
+                                     value=machine.Pin(pin).value())
+        else:
+            self.__pin = machine.Pin(pin, machine.Pin.OUT, value=value)
+        self.__last_val = self.__pin.value()
+
+    def state(self):
+        if self.__pin.value() == self.__on:
+            return 1
+        else:
+            return 0
+
+    def store(self):
+        self.__last_val = self.__pin.value()
+
+    def save(self):
+        save(self.__pin.value())
+
+    def on(self):
+        self.__pin.value(self.__on)
+        self.store()
+        return 1
+
+    def off(self):
+        self.__pin.value(self.__off)
+        self.store()
+        return 0
+
+    def toggle(self):
+        self.__pin.value(not self.__pin.value())
+        self.store()
+        return self.state()
+
+    def check(self):
+        if self.__pin.value() != self.__last_val:
+            self.toggle()
+#         print('[i] Relay.check: {0}.'.format(self.state()))
+        return self.state()
+
+    def switch(self, value):
+        if value is None or value in [2, '2', 'toggle']:
+            return self.toggle()
+        elif value in [0, '0', 'off']:
+            return self.off()
+        elif value in [1, '1', 'on']:
+            return self.on()
+        return self.state()
+
+
+class Led:
+    def __init__(self, pin=13, on=0, value=1):
+        self.__on = on
+        self.__off = 1 - on
+        self.__pin = machine.Pin(pin, machine.Pin.OUT, value=value)
+
+    def on(self):
+        self.__pin.value(self.__on)
+
+    def off(self):
+        self.__pin.value(self.__off)
+
+    def toggle(self):
+        self.__pin.value(not self.__pin.value())
+
+    def blink(self, count=1, delay=250):
+        self.off()
+        for i in range((count - 1) * 2 + 1):
+            self.toggle()
+            utime.sleep_ms(delay)
+        self.off()
+
+
+class Button:
+    def __init__(self, pin=0, on=0, callback=None):
+        self.__on = on
+        self.__off = 1 - on
+        self.__pin = machine.Pin(pin, machine.Pin.IN)
+        self.__callback = callback
+        if self.__callback is not None:
+            self.__pin.irq(trigger=machine.Pin.IRQ_FALLING,
+                           handler=self.__callback)
+
+    def set_callback(self, callback):
+        self.__callback = callback
+        if self.__callback is not None:
+            self.__pin.irq(trigger=machine.Pin.IRQ_FALLING,
+                           handler=self.__callback)
+
+    def state(self):
+        if self.__pin.value() == self.__on:
+            return 1
+        else:
+            return 0
+
 
 class Sonoff:
-  def __init__(self):
-    self.__led = Pin(_PIN_LED, Pin.OUT, value=(1-_LED_ON))
-    self.__button = Pin(_PIN_BTN, Pin.IN)
-    self.__relay = Pin(_PIN_REL, Pin.OUT, value=self.load())
+    def __init__(self, button_callback=None):
+        self.led = Led()
+        if button_callback is None:
+            self.button = Button(callback=self.button_callback)
+        else:
+            self.button = Button(callback=button_callback)
+        self.relay = Relay()
 
-    self.__mqtt = None
-    self.__mqtt_topic_in = None
-    self.__mqtt_topic_out = None
+        self.wlan = WLAN(WLAN_SSID, WLAN_PASS)
 
-  def load(self, save_file='relay.save'):
-    try:
-      if save_file in listdir('/'):
-        with open(save_file, 'r', encoding='utf-8') as save:
-          return int(save.readline())
-      else:
-        return Pin(_PIN_REL).value()
-    except Exception as e:
-      print('[-] Sonoff.load: Exception occured\n    {0}'.format(e))
-      return (1 - _REL_ON)
+        self.mqtt = None
+        self.mqtt_topic_in = MQTT_TPIN
+        self.mqtt_topic_out = MQTT_TPOU
 
-  def save(self, save_file='relay.save', value=0):
-    try:
-      with open(save_file, 'w', encoding='utf-8') as save:
-        save.write(str(value))
-      return True
-    except Exception as e:
-      print('[-] Sonoff.save: Exception occured\n    {0}'.format(e))
-      return False
+    def reset(self, save_state=True):
+        self.led.blink(1, 1000)
+        if save_state:
+            self.relay.save()
+        machine.reset()
 
-  def reset(self, save_state=True):
-    if save_state:
-      self.save(value=self.__relay.value())
-    reset()
+    def mqtt_publish(self, message):
+        if self.mqtt is not None:
+            try:
+                print('[i] Sonoff.mqtt_publish: {0} - {1}'.format(self.mqtt_topic_out.decode('utf-8'), message))
+                self.mqtt.publish(self.mqtt_topic_out, str(message).encode())
+            except KeyboardInterrupt:
+                raise
+            except Exception as e:
+                eprint('[-] Sonoff.mqtt_publish: {0}'.format(e))
+                self.mqtt.reconnect()
 
-  def blink(self, count=1, delay=250):
-    for i in range(count * 2 + 1):
-      self.__led.value(abs(_LED_ON - i % 2))
-      sleep_ms(delay)
+    def button_callback(self, pin):
+        bstart_ms = utime.ticks_ms()
+        while self.button.state() == 1:
+            utime.sleep_ms(100)
+        if utime.ticks_diff(bstart_ms, utime.ticks_ms()) > 1000:
+            self.reset()
+        else:
+            self.mqtt_publish(self.relay.toggle())
 
-  def switch(self, value=2):
-    if value in [0, '0']:       # off
-      self.__relay.value(1 - _REL_ON)
-    elif value in [1, '1']:     # on
-      self.__relay.value(_REL_ON)
-    elif value in [2, '2']:     # toggle
-      self.__relay.value(not self.__relay.value())
-    # any other case just returns state
-    if self.__relay.value() == _REL_ON:
-      return 1
-    else:
-      return 0
+    def mqtt_subscribe_callback(self, topic, msg):
+        topic = topic.decode('utf-8')
+        msg = msg.decode('utf-8')
+        print('[i] mqtt_subscribe_callback: {0} - {1}'.format(topic, msg))
+        if msg == 'reset':
+            self.reset()
+        elif msg == 'blink':
+            self.led.blink()
+        elif msg == 'state':
+            self.mqtt_publish(self.relay.state())
+        else:
+            self.mqtt_publish(self.relay.switch(msg))
 
-  def mqtt_subscribe_callback(self, topic, msg):
-    topic = topic.decode('utf-8')
-    msg = msg.decode('utf-8')
-    if msg == 'reset':
-      self.reset()
-    else:
-      self.publish(str(self.switch(msg)).encode())
+    def mqtt_start(self, mqtt_broker, mqtt_client_id, mqtt_port=1883):
+        self.mqtt_topic_in = str(self.wlan.get_hostname() + MQTT_TPIN).encode()
+        self.mqtt_topic_out = str(self.wlan.get_hostname() + MQTT_TPOU).encode()
+        try:
+            self.mqtt = MQTTClient(mqtt_client_id, mqtt_broker, mqtt_port)
+            self.mqtt.set_callback(self.mqtt_subscribe_callback)
+            self.mqtt.connect(clean_session=False)
+            print('[+] Sonoff.mqtt_start: Connected to ({0}'.format(str(mqtt_broker)))
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            eprint('[-] Sonoff.mqtt_setup: {0}'.format(e))
+            eprint('    Failed to connect to MQTT, resetting..')
+            self.reset()
+        try:
+            self.mqtt.subscribe(self.mqtt_topic_in)
+            print('[+] Sonoff.mqtt_start: Subscribed to {0}'.format(self.mqtt_topic_in.decode('utf-8')))
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            eprint('[-] Sonoff.mqtt_setup: {0}'.format(e))
+            eprint('    Failed to subscribe to {1}, resetting..'.format(self.mqtt_topic_in.decode('utf-8')))
+            self.reset()
+            self.led.blink(3)
 
-  def mqtt_setup(self, mqtt_broker, mqtt_client_id, mqtt_port=1883, topic_in='in', topic_out='out'):
-    self.__mqtt_topic_in   = topic_in.encode()
-    self.__mqtt_topic_out = topic_out.encode()
-    try:
-      self.__mqtt = MQTTClient(mqtt_client_id, mqtt_broker, mqtt_port)
-      self.__mqtt.set_callback(self.mqtt_subscribe_callback)
-      self.__mqtt.connect()
-    except KeyboardInterrupt:
-      raise
-    except Exception as e:
-      print('[-] Sonoff.mqtt_setup: {0}\n    Failed to connect to MQTT, resetting..'.format(e))
-      self.reset()
-    try:
-      self.__mqtt.subscribe(self.__mqtt_topic_in)
-    except KeyboardInterrupt:
-      raise
-    except Exception as e:
-      print('[-] Sonoff.mqtt_setup: {0}\n    Failed to subscribe to {1}, resetting..'.format(e, self.__mqtt_topic_in.decode('utf-8')))
-      self.reset()
+    def mqtt_stop(self):
+        self.mqtt.disconnect()
 
-  def mqtt_publish(self, message):
-    if not self.__mqtt is None:
-      try:
-        self.__mqtt.publish(self.__mqtt_topic_out, message.encode())
-      except KeyboardInterrupt:
-        raise
-      except Exception as e:
-        print('[-] Sonoff.mqtt_publish: {0}\n    Failed MQTT publish, resetting..'.format(e))
-        self.reset()
+    def run(self):
+        i = 0
+        j = 0
+        while True:
+            i += 1
+            try:
+                self.mqtt.check_msg()
+                self.relay.check()
+                if i % 100 == 0:
+                    print('[i] Sonoff.run: {0} - {1}'.format(self.mqtt_topic_out.decode('utf-8'), self.relay.state()))
+                    self.mqtt_publish(self.relay.check())
+                if i % 300 == 0:
+                    if not self.wlan.isconnected():
+                        self.wlan.connect(delay=WLAN_DLAY)
+                        self.led.blink(3)
+                    else:
+                        self.led.blink(1, 300)
+                if i % 36000 == 0:
+                    gc.collect()
+                utime.sleep_ms(100)
+
+            except KeyboardInterrupt:
+                raise
+
+            except Exception as e:
+                j += 1
+                eprint('[-] Sonoff.run: {0}'.format(e))
+                if j == 10:
+                    eprint('[-] Resetting...')
+                    self.reset()
+                else:
+                    eprint('[-] Running garbage collection...')
+                    gc.collect()
+
+
+s = Sonoff()
+s.mqtt_start(MQTT_BRKR, s.wlan.get_hostname(), MQTT_PORT)
+s.run()
